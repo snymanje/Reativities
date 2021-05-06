fd173d623e86145cee0e382649efd9be4ed291aa
jeansn <jeansn@tfg.co.za>
2021-05-06 18:46:44 +0200

    removed create-react-app

--- client-create-react-app/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
-import "./app/layout/styles.css";
-import App from "./app/layout/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
