2a5eea27c31d7cda58c8a3d6f11518c980efd6f0
jeansn <jeansn@tfg.co.za>
2021-05-07 07:50:52 +0200

    added store and context

--- client-vite/src/app/layout/App.tsx
+++ client-vite/src/app/layout/App.tsx
@@ -7,8 +7,11 @@ import NavBar from "./NavBar";
 import { v4 as uuid } from "uuid";
 import agent from "../api/agent";
 import Loading from "../components/Loading";
+import { useStore } from "../stores/store";
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -79,6 +82,7 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: "7em" }}>
+        <h2>{activityStore.title}</h2>
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
--- client-vite/src/main.tsx
+++ client-vite/src/main.tsx
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
+import { store, StoreContext } from "./app/stores/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
+  document.getElementById("root")
+);
